dbd5afd7ccb7a50b81388a851e756c486c125972
Traci <tas1013@gmail.com>
2021-03-19 16:38:12 -0500

    removed cached file

---
+++ server/index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+let Adult = require('./model');
+const PORT = 4000;
+
+const uri = 'mongodb://127.0.0.1:27017/capstone';
+//database integration
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch(error => console.log(error));
+
+mongoose.connect(uri, (err, db) => {
+  const user = new Adult({
+    firstName: 'Andrea',
+    lastName: 'Crabtree',
+    email: 'andrea@email.com',
+    child: [
+      {
+        firstName: 'Ben',
+        avatar: undefined,
+        totalPoints: undefined,
+        dailyPoints: undefined,
+      },
+      {
+        firstName: 'Julie',
+        avatar: undefined,
+        totalPoints: undefined,
+        dailyPoints: undefined,
+      },
+    ],
+  });
+  const user2 = new Adult({
+    firstName: 'Myra',
+    lastName: 'Kahn',
+    email: 'myra@email.com',
+    child: [
+      {
+        firstName: 'Maggie',
+        avatar: undefined,
+        totalPoints: undefined,
+        dailyPoints: undefined,
+      },
+      {
+        firstName: 'Sarah',
+        avatar: undefined,
+        totalPoints: undefined,
+        dailyPoints: undefined,
+      },
+    ],
+  });
+  user.save();
+  user2.save();
+  // db.collection('adults').insertMany([
+  //   {
+  //     firstName: 'Andrea',
+  //     lastName: 'Crabtree',
+  //     email: 'andrea@email.com',
+  //     child: [{ firstName: 'Ben' }],
+  //   },
+  //   {
+  //     firstName: 'Myra',
+  //     lastName: 'Kahn',
+  //     email: 'myra@email.com',
+  //     child: [{ firstName: 'Meg' }, { firstName: 'Luis' }],
+  //   },
+  // ]);
+});
+const connection = mongoose.connection;
+
+connection.once('open', function () {
+  console.log('Connection with MongoDB was successful');
+});
+
+//logging middelware
+app.use(cors());
+
+//bodyparsing middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/', (req, res, next) => {
+  res.send('hello world');
+});
+app.listen(PORT, function () {
+  console.log('Server is running on Port: ' + PORT);
+});
---
+++ server/model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const Schema = mongoose.Schema;
+
+let adultSchema = new Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+    validate: [validator.isEmail, 'Invalid email address'],
+  },
+  child: [
+    {
+      firstName: { type: String },
+      avatar: {
+        type: String,
+        default:
+          'https://images.assetsdelivery.com/compings_v2/drawkman/drawkman1709/drawkman170900284.jpg',
+        required: true,
+      },
+      totalPoints: {
+        type: Number,
+        default: 0,
+        required: true,
+      },
+      dailyPoints: {
+        type: Number,
+        default: 0,
+        required: true,
+      },
+    },
+  ],
+});
+
+const Adult = mongoose.model('Adult', adultSchema);
+module.exports = Adult;
