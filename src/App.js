05c8e642b5a9849b363a8af2540a98ccb3d0d1e3
Lindsay Welhoelter <lwelhoelter@gmail.com>
2021-03-23 09:04:17 -0400

    feat: added posenet component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Posenet from './Posenet';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Capstone Project</header>
+      <Posenet />
     </div>
   );
 }
