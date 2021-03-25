e59282c8079fcc98e269984a00f354e957f152e0
Lindsay Welhoelter <lwelhoelter@gmail.com>
2021-03-25 08:58:03 -0400

    feat: added navbar component

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav id="nav">
+        <Link to="/childdashboard">
+          <FontAwesomeIcon id="home" icon={faHome} />
+        </Link>
+      </nav>
+    </div>
+  );
+}
--- src/Components/index.js
+++ src/Components/index.js
@@ -2,3 +2,4 @@ export { default as Login } from './SigninElements/Login';
 export { default as Signup } from './SigninElements/Signup';
 export { default as Posenet } from './Exercises/Posenet';
 export { default as ChildDashboard } from './ChildDashboard';
+export { default as Navbar } from './Navbar';
