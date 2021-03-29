63c2c1cc0b69597a5e7beb66f0a1aa7343560636
Myra Khan <trueimpulse@yahoo.com>
2021-03-29 18:03:52 -0400

    feat(single exercise): updates

--- src/Components/ExerciseLibrary/SingleExercise.js
+++ src/Components/ExerciseLibrary/SingleExercise.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { Redirect } from 'react-router-dom';
-import * as tmPose from '@teachablemachine/pose';
-import { connect } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+import * as tmPose from "@teachablemachine/pose";
+import { connect } from "react-redux";
 
 //*********** UPDATE to {exercise.count}
 let totalCount = 5;
@@ -19,8 +19,8 @@ const SingleExercise = ({ match, selectedChild }) => {
   let model, webcam, ctx;
 
   async function init() {
-    const modelURL = URL + 'model.json';
-    const metadataURL = URL + 'metadata.json';
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
 
     // load the model and metadata
     // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
@@ -33,23 +33,23 @@ const SingleExercise = ({ match, selectedChild }) => {
     const flip = true; // whether to flip the webcam
     webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
 
-    await webcam.setup({ facingMode: 'user' });
-    let iosVid = document.getElementById('canvas');
+    await webcam.setup({ facingMode: "user" });
+    let iosVid = document.getElementById("canvas");
     iosVid.appendChild(webcam.webcam);
-    let videoElement = document.getElementsByTagName('video')[0];
-    videoElement.setAttribute('playsinline', true);
-    videoElement.muted = 'true';
-    videoElement.id = 'webcamVideo';
+    let videoElement = document.getElementsByTagName("video")[0];
+    videoElement.setAttribute("playsinline", true);
+    videoElement.muted = "true";
+    videoElement.id = "webcamVideo";
 
     // request access to the webcam
     await webcam.play();
     startAnimation = window.requestAnimationFrame(loop);
 
     // append/get elements to the DOM
-    const canvas = document.getElementById('canvas');
+    const canvas = document.getElementById("canvas");
     canvas.width = size;
     canvas.height = size;
-    ctx = canvas.getContext('2d');
+    ctx = canvas.getContext("2d");
   }
 
   async function loop(timestamp) {
@@ -65,19 +65,19 @@ const SingleExercise = ({ match, selectedChild }) => {
     // Prediction 2: run input through teachable machine classification model
     const prediction = await model.predict(posenetOutput);
 
-    let repContainer = document.getElementById('rep-container');
+    let repContainer = document.getElementById("rep-container");
 
     if (totalCount > 0) {
       if (prediction[0].probability.toFixed(2) >= 0.75) {
         if (prediction[0].className !== previousPose) {
           totalCount--;
-          console.log('Prediction 1: ', totalCount);
+          console.log("Prediction 1: ", totalCount);
           previousPose = prediction[0].className;
         }
       } else if (prediction[1].probability.toFixed(2) >= 0.75) {
         if (prediction[1].className !== previousPose) {
           totalCount--;
-          console.log('Prediction 2: ', totalCount);
+          console.log("Prediction 2: ", totalCount);
           repContainer.innerHTML = `You have ${Math.ceil(
             totalCount / 2
           )} left!`;
@@ -106,13 +106,13 @@ const SingleExercise = ({ match, selectedChild }) => {
 
   useEffect(() => {
     init();
-    console.log('USE EFFECT CALLED INSIDE SINGLE EXERCISE!!!');
+    console.log("USE EFFECT CALLED INSIDE SINGLE EXERCISE!!!");
     return function cleanup() {
       if (finishedExercise === true) setFinished(false);
       window.cancelAnimationFrame(startAnimation);
       window.cancelAnimationFrame(startAnimation2);
     };
-  }, [finishedExercise]);
+  }, []);
 
   return (
     <div>
@@ -128,7 +128,7 @@ const SingleExercise = ({ match, selectedChild }) => {
   );
 };
 
-const mapState = state => {
+const mapState = (state) => {
   return {
     selectedChild: state.selectedChild,
   };
