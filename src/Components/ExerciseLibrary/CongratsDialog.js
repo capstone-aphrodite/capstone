a7fe26ad0f7a48cbeb159ed1e1d4df6ccedc17ea
Traci <tas1013@gmail.com>
2021-03-31 14:15:11 -0500

    merge changes to branch

--- src/Components/ExerciseLibrary/CongratsDialog.js
+++ src/Components/ExerciseLibrary/CongratsDialog.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Typography, makeStyles } from '@material-ui/core';
-// import { isCompositeComponent } from 'react-dom/test-utils';
+
+const useStyles = makeStyles({
+  body: {
+    background: '#1ED882',
+    overflow: 'hidden',
+    color: 'white',
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '150%',
+    width: '100%',
+    padding: 10,
+    margin: 0,
+  },
+  items: {
+    margin: 3,
+  },
+  button: {
+    color: 'black',
+  },
+});
 
 export default function CongratsDialog(props) {
   console.log('DIALOG PROPS -->', props);
   const classes = useStyles();
-
   return (
     <div className={classes.body}>
       <Typography variant="h4" className={classes.items}>
@@ -37,25 +57,3 @@ export default function CongratsDialog(props) {
     </div>
   );
 }
-
-const useStyles = makeStyles({
-  body: {
-    background: '#1ED882',
-    overflow: 'hidden',
-    color: 'white',
-    display: 'flex',
-    flexFlow: 'column nowrap',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '150%',
-    width: '100%',
-    padding: 10,
-    margin: 0,
-  },
-  items: {
-    margin: 3,
-  },
-  button: {
-    color: 'black',
-  },
-});
